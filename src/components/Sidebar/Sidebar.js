0a2d8bb15656544b284d962a00c2adc5389c5fae
Vitaly Aminev <v@aminev.me>
2016-07-27 00:47:20 +0300

    feat: sidebar styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,16 +5,23 @@ import MenuItemLink from 'components/MenuItemLink';
 
 const Sidebar = () => (
   <Drawer open containerStyle={styles.base}>
-    <MenuItemLink to="/">Player</MenuItemLink>
-    <MenuItemLink to="/inventory">Inventory</MenuItemLink>
-    <MenuItemLink to="/settings">Settings</MenuItemLink>
-    <MenuItemLink to="/map">Map</MenuItemLink>
+    <div style={styles.separator} />
+    <MenuItemLink to="/" icon={require('../../../assets/icons/profile.svg')} />
+    <MenuItemLink to="/inventory" icon={require('../../../assets/icons/pokeball.svg')} />
+    <MenuItemLink to="/map" icon={require('../../../assets/icons/map.svg')} />
+    <MenuItemLink to="/settings" icon={require('../../../assets/icons/settings.svg')} />
   </Drawer>
 );
 
 const styles = {
   base: {
-    width: 100
+    width: 70,
+    background: '#2b303b'
+  },
+  separator: {
+    height: 64,
+    background: '#272B35',
+    marginBottom: '20px'
   }
 };
 
